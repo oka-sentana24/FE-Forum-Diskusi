535a348477f94dc43f3a6654eed515a06078aade
oka sentana <oka.sentana@taksuteknologi.com>
2022-01-31 14:43:54 +0800

    first-commit

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  let count: number = 0;
+
+  const increment = () => {
+    count += 1;
+  };
+</script>
+
+<button class="test" on:click={increment}>
+  Clicks: <span class="testclass">{count}</span>
+</button>
+
+<style>
+  .testclass {
+    @apply font-light text-green-500;
+  }
+</style>
