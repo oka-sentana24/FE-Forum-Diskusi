296b44022a82ec8752d8570b91f9bb64631306f0
oka sentana <oka.sentana@taksuteknologi.com>
2022-02-22 22:33:26 +0800

    create sidebar

--- src/routes/admin/$layout.svelte
+++ src/routes/admin/$layout.svelte
@@ -1,10 +1,24 @@
 <script>
-	import Sidebar from '$lib/Sidebar.svelte';
-	import SidebarMini from '$lib/Sidebar-mini.svelte';
+	import Sidebar from '$lib/Sidebar/sidebar.svelte';
+	import SidebarMini from '$lib/Sidebar/sidebar-mini.svelte';
 	import HeaderAdmin from '$lib/Header/admin.svelte';
 </script>
 
-<div class="main-admin">
+<main class="admin">
+	<div class="md:flex md:flex-cols-2 md:gap-5">
+		<div class="flex flex-cols-2">
+			<div class="mini-side">
+				<SidebarMini />
+			</div>
+			<div class="sidebar">
+				<Sidebar/>
+			</div>
+		</div>
+		<div>dddddd</div>
+	</div>
+</main>
+
+<!-- <div class="main-admin">
 	<div class="admin-content">
 		<div class="mini-side">
 				<SidebarMini />
@@ -23,7 +37,7 @@
 				<slot/>
 			</div>
 		</div>
-	</div>
+	</div> -->
 	
 	<!-- <div>
 		<Sidebar />
@@ -40,4 +54,4 @@
 			<slot />
 		</div>
 	</div> -->
-</div>
+<!-- </div> -->
---
+++ src/routes/admin/kelas/index.svelte
@@ -0,0 +1,72 @@
+<script>
+  // @ts-nocheck
+    import {
+      DataTable,
+      DataTableHead,
+      DataTableRow,
+      DataTableCell,
+      DataTableBody,
+      Button,
+      Row, Col, Select,Checkbox
+   } from 'svelte-materialify';
+   import Link from "svelte-link";
+    let checked = false;
+    let group = [1,2,3,4,5]
+
+  const items = [
+    { name: 'Delete', value: 'Delete' },
+  ];
+
+  let columns = ["Nisn", "Nama", "Jurusan", "Kelas", "Alamat", "Jenis Kelamin", "Tempat lahir", "Agama", "No tlp", "Email"]
+	let data = [
+    ["170030299", "john@example.com", "(353) 01 222 3333","john@example.com", "(353) 01 222 3333","john@example.com", "(353) 01 222 3333","(353) 01 222 3333","john@example.com", "(353) 01 222 3333"],
+    ["170030299", "john@example.com", "(353) 01 222 3333","john@example.com", "(353) 01 222 3333","john@example.com", "(353) 01 222 3333","(353) 01 222 3333","john@example.com", "(353) 01 222 3333"],
+    ["170030299", "john@example.com", "(353) 01 222 3333","john@example.com", "(353) 01 222 3333","john@example.com", "(353) 01 222 3333","(353) 01 222 3333","john@example.com", "(353) 01 222 3333"],
+    ["170030299", "john@example.com", "(353) 01 222 3333","john@example.com", "(353) 01 222 3333","john@example.com", "(353) 01 222 3333","(353) 01 222 3333","john@example.com", "(353) 01 222 3333"],
+    ["170030299", "john@example.com", "(353) 01 222 3333","john@example.com", "(353) 01 222 3333","john@example.com", "(353) 01 222 3333","(353) 01 222 3333","john@example.com", "(353) 01 222 3333"],
+    
+  ]
+
+  //Checkall Checkbox
+
+
+  </script>
+
+  <div class="main-siswa">
+    <div class="flex flex-cols-2 justify-between py-5 items-center">
+      <div class="filter">
+        <Row>
+          <Col>
+            <Select solo items={items} placeholder="Filter"/>
+          </Col>
+        </Row>
+      </div>
+      <div class="create">
+        <Button><a href="/admin/siswa/create">
+          New siswa
+        </a></Button>
+      </div>
+    </div>
+    <div class="main-tabel">  
+        <DataTable>
+          <DataTableHead>
+            <DataTableRow>
+              {#each columns as column}
+              <DataTableCell>{column}</DataTableCell>
+              {/each}
+            </DataTableRow>
+          </DataTableHead>
+          <DataTableBody>
+            {#each data as row}
+            <DataTableRow>
+                {#each row as cell}
+                  <DataTableCell>
+                    {cell}
+                  </DataTableCell>
+                  {/each}
+            </DataTableRow>
+            {/each}
+          </DataTableBody>
+        </DataTable>
+    </div>
+  </div>
